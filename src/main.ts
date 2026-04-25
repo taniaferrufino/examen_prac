18c847a8efa29fd118b958d87aee4083548f2ae7
Tania Ferrufino <you@example.com>
2026-04-24 15:24:34 -1200

    examen

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
 
+  //Configuración de CORS (Vital para que el frontend se conecte
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true
     }),
   );
 
@@ -22,6 +26,12 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`API ejecutándose en el puerto: ${port}`);
+  console.log(`Documentación disponible en: /api/docs`);
 
   await app.listen(process.env.PORT ?? 3000);
 }
